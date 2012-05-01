4d1e7fafc61df6f547e7be9c460f1cc7a2320aef
uzulla <uzulla@himitsukichi.com>
2012-05-02 02:35:38 +0900

    update post table schema

--- db/init.sql
+++ db/init.sql
@@ -2,7 +2,9 @@ DROP TABLE IF EXISTS `post`;
 CREATE TABLE IF NOT EXISTS `post` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `text` text NOT NULL,
-  `by` text NOT NULL,
+  `nickname` varchar(32) NOT NULL,
+  `user_key` varchar(64) NOT NULL,
+  `profile_image_url` varchar(128) NOT NULL,
   `created_at_ms` bigint(20) unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
