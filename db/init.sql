5e5384266bfc754267e0fb0d52eeaf4e09c0fb22
uzulla <uzulla@himitsukichi.com>
2012-05-01 23:41:42 +0900

    update init.sql. add user table for user login.

--- db/init.sql
+++ db/init.sql
@@ -6,3 +6,16 @@ CREATE TABLE IF NOT EXISTS `post` (
   `created_at_ms` bigint(20) unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_key` varchar(64) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `nickname` varchar(32) NOT NULL,
+  `profile_image_url` varchar(128) NOT NULL,
+  `sns_data_cache` blob NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`user_key`),
+  UNIQUE KEY `token` (`token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
