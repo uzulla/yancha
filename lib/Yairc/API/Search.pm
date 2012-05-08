c325dcb7e70c25c3075517d9386caa5385b5c9eb
Takayuki Otake <k.wisiiy@gmail.com>
2012-05-08 23:21:16 +0900

    API::Search URLが含まれる発言のみを抽出するフィルターを追加

--- lib/Yairc/API/Search.pm
+++ lib/Yairc/API/Search.pm
@@ -44,7 +44,24 @@ sub _search_posts {
 
     $attr->{ limit } ||= 20;
 
-    return $self->data_storage->search_post( $where, $attr );
+    my $posts = $self->data_storage->search_post( $where, $attr );
+
+    # if文のネストを浅くしたい
+    if ( my $filter = $req->param('filter') ) {
+        if ( $filter eq 'url' ) {
+            my @filtered_posts = ();
+            foreach my$post( @$posts ) {
+                # regex comes from public/js/main.js 
+                if ($post->{ text } =~ m.https?://([\x21-\x7e]+).gi) {
+                    push(@filtered_posts, $post);
+                }
+            }
+
+            return \@filtered_posts;
+        }
+    }
+
+    return $posts;
 }
 
 
