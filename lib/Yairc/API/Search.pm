4cdede629da19ee61983cd8b389843d588289928
Takayuki Otake <k.wisiiy@gmail.com>
2012-05-08 23:48:05 +0900

    if文のネストを解消

--- lib/Yairc/API/Search.pm
+++ lib/Yairc/API/Search.pm
@@ -47,18 +47,17 @@ sub _search_posts {
     my $posts = $self->data_storage->search_post( $where, $attr );
 
     # if文のネストを浅くしたい
-    if ( my $filter = $req->param('filter') ) {
-        if ( $filter eq 'url' ) {
-            my @filtered_posts = ();
-            foreach my$post( @$posts ) {
-                # regex comes from public/js/main.js 
-                if ($post->{ text } =~ m.https?://([\x21-\x7e]+).gi) {
-                    push(@filtered_posts, $post);
-                }
+    my $filter = $req->param('filter') || '';
+    if ( $filter eq 'url' ) {
+        my @filtered_posts = ();
+        foreach my$post( @$posts ) {
+            # regex comes from public/js/main.js 
+            if ($post->{ text } =~ m.https?://([\x21-\x7e]+).gi) {
+                push(@filtered_posts, $post);
             }
-
-            return \@filtered_posts;
         }
+
+        return \@filtered_posts;
     }
 
     return $posts;
