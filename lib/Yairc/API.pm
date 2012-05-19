a9bf779c0784137c3fa4a26a696c9a106a161820
makamaka <makamaka.donzoko@gmail.com>
2012-05-19 12:15:56 +0900

    APIエンドポイントの自動生成

--- lib/Yairc/API.pm
+++ lib/Yairc/API.pm
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 use Encode;
 use JSON;
+use Plack::Response;
 
 our $VERSION = '0.01';
 
@@ -12,25 +13,44 @@ sub new {
     bless { @args }, $class;
 }
 
-sub data_storage { $_[0]->{ data_storage } } 
+sub sys { $_[0]->{ sys } }
+
+sub build_psgi_endpoint {
+    my ( $self, $opt ) = @_;
+
+    require Plack::Builder;
+    # Plack::Builder->import(); # why it does not export 'builder'?
+
+    return Plack::Builder::builder {
+        sub {
+            my $env = shift;
+            my $req = Plack::Request->new($env);
+            my $res = $self->run($req, $opt);
+            return $res->finalize;
+        };
+    };
+
+}
 
 sub response_as_json {
-    my ($self, $data) = @_;
+    my ($self, $data, $code) = @_;
+    my $res = Plack::Response->new($code || 200);
 
-    return {
-        'data'          => encode_json($data),
-        'content-type'  => 'application/json',
-    };
+    $res->content_type( 'application/json' );
+    $res->body( encode_json($data) );
+
+    return $res;
 }
 
 sub response_as_text {
-    my ($self, $data) = @_;
+    my ($self, $data, $code) = @_;
+    my $res = Plack::Response->new($code || 200);
 
-    return {
-        'data'          => join( "\n", map { Encode::encode_utf8( join(',', values %$_ ) ) } @$data ),
-        'content-type'  => 'text/plain',
-    };
+    $res->content_type( 'text/plain' );
+    $res->body( join( "\n", map { Encode::encode_utf8( join(',', values %$_ ) ) } @$data ) );
+
+    return $res;
 }
 
-
 1;
+
