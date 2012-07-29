e463cdfdfb0fa7d5330f63ca9c081752348ef23e
Takayuki Otake <k.wisiiy@gmail.com>
2012-07-29 17:07:17 +0900

    #56 Twitter認証でログインしてるユーザーにソーシャルアイコンを表示

--- public/js/user.js
+++ public/js/user.js
@@ -13,15 +13,24 @@ function joinMembers () {
         var profile_image_url = (data[i].profile_image_url !== '') ? data[i].profile_image_url : 'nobody.png';
         var snsUrl = getSnsProfileUrlByUserkey(data[i]);
 
-        //要素の横幅調整。画像の横幅より名前の横幅の方が長ければ名前の文字分の横幅にする
         //todo: マルチバイト文字列の場合の文字数計算
         var block_width = (data[i].nickname.length/2) * 16;
-        if (block_width < 64) {
-            block_width = 64;
+        if (block_width < 16*4) {
+            block_width = 16*5;
+        }
+        else {
+            block_width += 16;
         }
 
         if (snsUrl !== '') {
-            joinUsers.append('<div class="join_member" style="width:' + block_width + 'px;"><a href="' + snsUrl + '"><img class="profile_url" src="' + profile_image_url + '"><br>' + data[i].nickname + '</a></div>');
+            joinUsers.append(
+                '<div class="join_member" style="width:' + block_width + 'px;">' +
+                '<a href="' + snsUrl + '"><img class="profile_url" src="' + profile_image_url + '">' +
+                '<br style="clear:both;">' + 
+                '<img src="' + getSnsIconPath(data[i].user_key.split(':')[0]) + '">' + 
+                data[i].nickname + 
+                '</a></div>'
+            );
         }
         else {
             joinUsers.append('<div class="join_member" style="width:' + block_width + 'px;"><img class="profile_url" src="/img/' + profile_image_url + '"><br>' + data[i].nickname + '</div>');
@@ -43,3 +52,15 @@ function getSnsProfileUrlByUserkey(userData) {
     }
 }
 
+function getSnsIconPath(sns) {
+    var path = '/img/sns/';
+    var image_name = '';
+    var extension  = '';
+
+    switch (sns) {
+        case 'twitter':
+            image_name = 'twitter.png';
+    }
+
+    return path + image_name;
+}
