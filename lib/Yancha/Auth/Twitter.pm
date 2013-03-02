5908e04b18ed79f5924268834b9d8aea930e89f3
Junichi Ishida <uzulla@himitsukichi.com>
2013-03-02 22:25:45 +0900

    change twitter auth lib to AnyEvent::Twitter

--- lib/Yancha/Auth/Twitter.pm
+++ lib/Yancha/Auth/Twitter.pm
@@ -14,6 +14,8 @@ use Carp;
 use FindBin;
 use lib ("$FindBin::Bin/lib");
 use Net::Twitter::Lite;
+use AnyEvent::Twitter;
+use Data::Dumper;
 
 sub build_psgi_endpoint {
     my ( $self, $opt ) = @_;
@@ -34,24 +36,32 @@ sub build_psgi_endpoint {
         mount '/start' => builder {
             sub {
                 my $env     = shift;
-                my $req     = Plack::Request->new($env);
-                my $session = Plack::Session->new( $env );
                 my $ret_url = $self->redirect_url( $env, "$endpoint/callback" );
-                my $url     = try { 
-                    $nt->get_authorization_url( callback => $ret_url );
-                } catch {
-                    Carp::croak( 
-                        sprintf("Authorization Error:\n\t%s\tCheck your consumer-key and consumer-secret in config.pl\n ", $_) 
+                
+                return sub {
+                    my $responder = shift;
+                    my $headers = [];
+                    my $req     = Plack::Request->new($env);
+                    my $session = Plack::Session->new($env);
+
+                    AnyEvent::Twitter->get_request_token(
+                        consumer_key    => $opt->{ consumer_key },
+                        consumer_secret => $opt->{ consumer_secret },
+                        callback_url    => $ret_url,
+                        auth => 'authenticate',
+                        cb => sub {
+                            my ($location, $response, $body, $header) = @_;
+                            
+                            $session->set( 'token', $response->{oauth_token});
+                            $session->set( 'token_secret', $response->{oauth_token_secret} );
+                            $session->set( 'token_only', 1 ) if $req->parameters->{ token_only };
+
+                            my $res = Plack::Response->new;
+                            $res->redirect($location);
+                            $responder->($res->finalize);
+                        },
                     );
                 };
-
-                $session->set( 'token', $nt->request_token );
-                $session->set( 'token_secret', $nt->request_token_secret );
-                $session->set( 'token_only', 1 ) if $req->parameters->{ token_only };
-
-                my $res = Plack::Response->new;
-                $res->redirect($url);
-                return $res->finalize;
             }
         };
 
@@ -68,37 +78,72 @@ sub build_psgi_endpoint {
                     return [ 401, ['Content-Type' => 'text/html',], ["session lost"] ];
                 }
 
-                my $token = '';
+                my $_token = '';
                 unless ( $req->param('denied') ) {
-                    $nt->request_token( $session->get('token') );
-                    $nt->request_token_secret( $session->get('token_secret') );
                     my $verifier = $req->param('oauth_verifier');
-                    ( $access_token, $access_token_secret, $user_id, $screen_name ) =
-                                          $nt->request_access_token( verifier => $verifier );
+                    
+                    return sub {
+                      my $responder = shift;
+                      my $session = Plack::Session->new( $env );
+                      
+                      AnyEvent::Twitter->get_access_token(
+                        consumer_key       => $opt->{ consumer_key },
+                        consumer_secret    => $opt->{ consumer_secret },
+                        oauth_token        => $session->get('token'),
+                        oauth_token_secret => $session->get('token_secret'),
+                        oauth_verifier     => $verifier,
+                        cb => sub {
+                          my ($__token, $body, $header) = @_;
+                          
+                          my $at = AnyEvent::Twitter->new(
+                            consumer_key       => $opt->{ consumer_key },
+                            consumer_secret    => $opt->{ consumer_secret },
+                            access_token        => $__token->{oauth_token},
+                            access_token_secret => $__token->{oauth_token_secret},
+                          );
+                          
+                          $at->get('account/verify_credentials', sub {
+                            my ($header, $response, $reason) = @_;
+                            
+                            my $user    = $self->set_user_into_storage( {
+                              user_key          => 'twitter:' . $response->{id},
+                              nickname          => $response->{screen_name},
+                              profile_image_url => $response->{profile_image_url},
+                              profile_url       => 'https://twitter.com/#!/'.$response->{screen_name},
+                              sns_data_cache    => encode_json($response),
+                            } );
+                            my $_token = $user->{ token };
+                            
+                            if($session->remove( 'token_only' )){
+                              $responder->($self->response_token_only($_token)->finalize);
+                            }else{
+                              my $res = Plack::Response->new();
+                              my $ret_url = $self->redirect_url( $env );
+                              $res->redirect( $ret_url );
+                              $res->cookies->{yancha_auto_login_token} = {
+                                  value => $_token,
+                                  path  => "/",
+                                  expires => time + 24 * 60 * 60,
+                              };
+                              $responder->($res->finalize);
+                            }
+                          });
+                        },
+                      );
+                    }
+                
+                }else{
 
-                    my $profile = eval { $nt->verify_credentials() };
-                    my $user    = $self->set_user_into_storage( {
-                        user_key          => 'twitter:' . $profile->{id},
-                        nickname          => $screen_name,
-                        profile_image_url => $profile->{profile_image_url},
-                        profile_url       => 'https://twitter.com/#!/'.$screen_name,
-                        sns_data_cache    => encode_json($profile),
-                    } );
-                    $token = $user->{ token };
-
-                    return $self->response_token_only($token)->finalize
-                                            if $session->remove( 'token_only' );
+                  my $res = Plack::Response->new();
+                  my $ret_url = $self->redirect_url( $env );
+                  $res->redirect( $ret_url );
+                  $res->cookies->{yancha_auto_login_token} = {
+                      value => $_token,
+                      path  => "/",
+                      expires => time + 24 * 60 * 60,
+                  };
+                  return $res->finalize;
                 }
-
-                my $res = Plack::Response->new();
-                my $ret_url = $self->redirect_url( $env );
-                $res->redirect( $ret_url );
-                $res->cookies->{yancha_auto_login_token} = {
-                    value => $token,
-                    path  => "/",
-                    expires => time + 24 * 60 * 60,
-                };
-                return $res->finalize;
             };
         };
     };
@@ -109,3 +154,4 @@ sub build_psgi_endpoint {
 
 
 1;
+
