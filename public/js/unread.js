58206c5dbef37753f3e8c6f672c46b81776267c2
Kotaro Kawashima <utageworks+gh@gmail.com>
2013-05-19 14:00:11 +0900

    JSLint先生に許してもらえるまで殴った #105

--- public/js/unread.js
+++ public/js/unread.js
@@ -12,10 +12,10 @@ var unreadManager = {};
  * 未読セルを抽出し、そのセルに対して、既読判定をする
  * @function
  */
-unreadManager.unreadcheck = function(){
+unreadManager.unreadcheck = function() {
 	var unreads = $("#lines").find(".unread");
 	unreads.each(unreadManager.unread2read);
-}
+};
 
 /**
  * 未読セルに対し、画面に表示されていたら既読処理をする
@@ -23,8 +23,8 @@ unreadManager.unreadcheck = function(){
  * @param {Number} index セルのインデックス。eachメソッドの既定引数として用意しているだけなので、使用していない
  * @param {Dom} cell 1つの未読セル。このセルに対して既読処理を行う
  */
-unreadManager.unread2read = function(index, cell){
-	var cell = $(cell);
+unreadManager.unread2read = function(index, cell_dom) {
+	var cell = $(cell_dom);
 	var bgcolor = cell.css("backgroundColor");
 	var ypos = cell.offset().top + cell.height();
 	if(ypos > 0 && ypos < unreadManager.lines.height()){
@@ -37,7 +37,7 @@ unreadManager.unread2read = function(index, cell){
 			updateTitle();
 		});
 	}
-}
+};
 
 /**
  * スクロール時に未読状態の判定をするメソッドを呼ぶようにする
@@ -46,7 +46,7 @@ unreadManager.unread2read = function(index, cell){
 unreadManager.init = function(){
 	unreadManager.lines = $("#lines");
 	unreadManager.lines.on("scroll", unreadManager.unreadcheck);
-}
+};
 
 $(function(){
 	unreadManager.init();
