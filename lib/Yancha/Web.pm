d8b4f2963d973bb21d7717b0e6f1f31320f5f6a4
Takayuki Otake <k.wisiiy@gmail.com>
2013-06-06 23:49:54 +0900

    WAFを導入してたくさん不都合があったので修正
    URLの下位互換
    quotが正しく表示されない #115

--- lib/Yancha/Web.pm
+++ lib/Yancha/Web.pm
@@ -11,31 +11,37 @@ path '/' => sub {
 
     my $template;
     if (Yancha::Util->is_smartphone( $req->env->{HTTP_USER_AGENT} || '' )) {
-        $template = 'chat_sp.tx';
-    } else {
-        $template = 'chat.tx';
+        return res { redirect('/android.html'); };
     }
 
+    $template = 'chat.tx';
+
     return {
-        template => $template,
+        template => 'chat.tx',
     };
 };
 
-path '/about' => sub {
+path '/android.html' => sub {
+    return {
+        template => 'android.tx',
+    };
+};
+
+path '/about(\.html)?' => sub {
     my $req = shift;
     return {
         template => 'about.tx',
     };
 };
 
-path '/hints' => sub {
+path '/hints(\.html)?' => sub {
     my $req = shift;
     return {
         template => 'hints.tx',
     };
 };
 
-path '/join_users' => sub {
+path '/join_users(\.html)?' => sub {
     my $req = shift;
     return {
         template => 'join_users.tx',
@@ -43,29 +49,13 @@ path '/join_users' => sub {
 };
 
 
-# 下位互換のためにquotation.htmlを残しておく
 path qr{^/quot(ation\.html)?$} => sub {
-    my $req = shift;
-
-    my $where;
-    my $posts = [];
-    if ( my $ids = $req->param('id') ) {
-        $where->{ id } = [ grep { $_ =~ /^[0-9]+$/ } split /,/, $ids ];
-        $posts = config->{app}->data_storage->search_post( $where, {order_by => 'created_at_ms ASC'} );
-
-        for my$p( @$posts ) {
-            $p->{profile_image_url} = config->{view}->{function}->{static}('/img/nobody.png') unless $p->{profile_image_url};
-        }
-    }
-
     return {
         template => 'quot.tx',
-        posts    => $posts,
-        post_count => scalar(@$posts),
     };
 };
 
-path '/search' => sub {
+path '/search(\.html)?' => sub {
     my $req = shift;
     return {
         template => 'search.tx',
