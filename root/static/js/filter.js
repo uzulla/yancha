28f038543cbbfa7d44799e1c10026471e031f143
uzulla <uzulla@himitsukichi.com>
2013-06-16 02:20:06 +0900

    bugfix: Pyazo inline inflate filter.

--- root/static/js/filter.js
+++ root/static/js/filter.js
@@ -1,11 +1,12 @@
 function user_message_filter(message){
 
     //auto inline display(pyazo)
-    message = message.replace(/http(s)?:\/\/yairc.cfe.jp:5000(\/[\x21-\x7e]+)\.(png|gif|jpg|jpeg)/gi,
-                              "<a href='//yairc.cfe.jp$2' target='_blank'><img src='//yairc.cfe.jp$2' style='max-width:300px;max-height:300px;'/></a>");
-
     message = message.replace(/http(s)?:\/\/yairc.cfe.jp(:5000)?(\/[\x21-\x7e]+)\.(png|gif|jpg|jpeg)/gi,
-		                                  "<a href='//yairc.cfe.jp$2' target='_blank'>//yairc.cfe.jp$2</a>");
+                              "<a href='//yairc.cfe.jp$3.$4' target='_blank'><img src='//yairc.cfe.jp$3.$4' style='max-width:300px;max-height:300px;'/></a>");
+
+    message = message.replace(/http(s)?:\/\/yairc.cfe.jp(:5000)?(\/[\x21-\x7e]+)\.^(png|gif|jpg|jpeg)/gi,
+                                                  "<a href='//yairc.cfe.jp$3.$4' target='_blank'>//yairc.cfe.jp$3.$4</a>");
+
     // YT thumbnail
     message = message.replace(/http(s)?:\/\/www.youtube.com\/[\x21-\x7e]*v=([a-zA-Z0-9\-]+)/g,
                               "<img src='//i1.ytimg.com/vi/$2/default.jpg'><br />http://www.youtube.com/watch?v=$2");
