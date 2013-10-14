2a37f0f0926c4c9a7d5082f7620f413d22a1731f
uzulla <uzulla@himitsukichi.com>
2013-10-14 12:41:06 +0900

    入力欄拡大修正

--- static/js/main_android.js
+++ static/js/main_android.js
@@ -37,7 +37,9 @@ function resizeMessageTextarea(linenum){
     linenum=10; // hard limit
   }
 
-  var em = (linenum * 1.2) + 'em';
+  char_num = $("#message").val().length/40;
+
+  var em = ((linenum * 1.2) + (char_num*1.2)) + 'em';
   $("#message").css('height', em);
   $(window).resize();
 }
