5c11734d4370dccbf9ee44f8f2869c5ed9ab89bf
ichigotake <k.wisiiy@gmail.com>
2013-10-22 22:12:17 +0900

    メッセージフィルターをプラガブルにしましたよっ

--- static/js/filter.js
+++ static/js/filter.js
@@ -1,5 +1,27 @@
-function user_message_filter(message){
+var filterPlugins = filterPlugins || [];
 
+var defaultFilterPlugins = [
+    filterPluginPyazoAutoInlineDisplay,
+    filterPluginGistInline,
+    filterPluginAsamashiate,
+    filterPluginTwitterInline,
+    filterPluginYoutubeThumbnail,
+    filterPluginAutoLink,
+    filterPluginSyntaxHilight,
+];
+
+for(var i in defaultFilterPlugins){
+    filterPlugins.push(defaultFilterPlugins[i]);
+}
+
+function user_message_filter(message){
+    for(var i in filterPlugins){
+        message = filterPlugins[i](message);
+    }
+    return message;
+}
+
+function filterPluginPyazoAutoInlineDisplay(message) {
     //auto inline display(pyazo)
     message = message.replace(/http(s)?:\/\/yairc.cfe.jp(:5000)?(\/[\x21-\x7e]+)\.(png|gif|jpg|jpeg)/gi,
                               "<a href='//yairc.cfe.jp$3.$4' target='_blank'><img src='//yairc.cfe.jp$3.$4' style='max-width:300px;max-height:300px;'/></a>");
@@ -7,11 +29,16 @@ function user_message_filter(message){
     message = message.replace(/http(s)?:\/\/yairc.cfe.jp(:5000)?(\/[\x21-\x7e]+)\.^(png|gif|jpg|jpeg)/gi,
                                                   "<a href='//yairc.cfe.jp$3.$4' target='_blank'>//yairc.cfe.jp$3.$4</a>");
 
-    //gist inline
-    message = message.replace(/https:\/\/gist.github.com\/[a-zA-Z0-9\-_]{1,30}\/([0-9a-zA-Z]{1,30})/g, function(whole,s1) {
+    return message;
+}
+
+function filterPluginGistInline(message){
+    return message.replace(/https:\/\/gist.github.com\/[a-zA-Z0-9\-_]{1,30}\/([0-9a-zA-Z]{1,30})/g, function(whole,s1) {
         return( 'https://gist.github.com/'+s1+'/ <br><iframe data-gist-id="'+s1+'" style="width:100%;"></iframe><script>load_gist("'+s1+'")</script><br>' );
     });
+}
 
+function filterPluginAsamashiate(message) {
     //アサマシエイト （サーバー運営費に当てます！！！）
     message = message.replace(/\/\/www\.amazon\.co\.jp[\x21-\x7e]*\/dp\/([0-9A-Z]{10,13})[\x21-\x7e]*/ig, function(whole,s1) {
         return( whole+' <br><iframe src="//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&t=uzulla-22&o=9&p=8&l=as4&m=amazon&f=ifr&ref=ss_til&asins='+s1+'" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>' );
@@ -19,19 +46,28 @@ function user_message_filter(message){
     message = message.replace(/\/\/www\.amazon\.co\.jp[\x21-\x7e]*\/gp\/product\/([0-9A-Z]{10,13})[\x21-\x7e]*/ig, function(whole,s1) {
         return( whole+' <br><iframe src="//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&t=uzulla-22&o=9&p=8&l=as4&m=amazon&f=ifr&ref=ss_til&asins='+s1+'" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>' );
     });
+    return message;
+}
 
+function filterPluginTwitterInline(message){
     //twitter inline https://twitter.com/uzulla/status/389391040480051200
-    message = message.replace(/http[s]*:(\/\/twitter.com\/[a-zA-Z0-9\-_]{1,40}\/status[e|s]*\/[0-9]{1,40})/g, function(whole,s1) {
+    return message.replace(/http[s]*:(\/\/twitter.com\/[a-zA-Z0-9\-_]{1,40}\/status[e|s]*\/[0-9]{1,40})/g, function(whole,s1) {
         return( 'https:'+s1+' <br><blockquote class="twitter-tweet"><a href="'+s1+'"></blockquote><script>twttr.widgets.load();</script>' );
     });
+}
 
+function filterPluginYoutubeThumbnail(message){
     // YT thumbnail
-    message = message.replace(/http(s)?:\/\/www.youtube.com\/[\x21-\x7e]*v=([a-zA-Z0-9\-]+)/g,
+    return message.replace(/http(s)?:\/\/www.youtube.com\/[\x21-\x7e]*v=([a-zA-Z0-9\-]+)/g,
                               "<img src='//i1.ytimg.com/vi/$2/default.jpg'><br />http://www.youtube.com/watch?v=$2");
+}
 
+function filterPluginAutoLink(message){
     //auto link
-    message = message.replace(/(http(s)?:\/\/[\x21-\x7e]+)/gi, "<a href='$1' target='_blank'>$1</a>");
+    return message.replace(/(http(s)?:\/\/[\x21-\x7e]+)/gi, "<a href='$1' target='_blank'>$1</a>");
+}
 
+function filterPluginSyntaxHilight(message) {
     message = message.replace(/&#62;\|javascript\|\n([\s\S]*?)\n\|\|&#60;/g, function(whole,s1) {
         return( '<pre class="sh_javascript">' + s1 + '</pre>' );
     });
@@ -81,7 +117,6 @@ function user_message_filter(message){
     });
 
     return message;
-
 }
 
 function load_gist(gistid){
