ea491b887451b5ceabf368cc477009a5efa47c97
ichigotake <k.wisiiy@gmail.com>
2013-10-22 22:34:30 +0900

    s/filterPlugin/messageFilter/g いい感じに改名忘れ

--- static/js/filter.js
+++ static/js/filter.js
@@ -1,12 +1,12 @@
 var messageFilters = messageFilters || [];
 
-messageFilters.push(filterPluginPyazoAutoInlineDisplay);
-messageFilters.push(filterPluginGistInline);
-messageFilters.push(filterPluginAsamashiate);
-messageFilters.push(filterPluginTwitterInline);
-messageFilters.push(filterPluginYoutubeThumbnail);
-messageFilters.push(filterPluginAutoLink);
-messageFilters.push(filterPluginSyntaxHilight);
+messageFilters.push(messageFilterPyazoAutoInlineDisplay);
+messageFilters.push(messageFilterGistInline);
+messageFilters.push(messageFilterAsamashiate);
+messageFilters.push(messageFilterTwitterInline);
+messageFilters.push(messageFilterYoutubeThumbnail);
+messageFilters.push(messageFilterAutoLink);
+messageFilters.push(messageFilterSyntaxHilight);
 
 function user_message_filter(message){
     for(var i in messageFilters){
@@ -15,7 +15,7 @@ function user_message_filter(message){
     return message;
 }
 
-function filterPluginPyazoAutoInlineDisplay(message) {
+function messageFilterPyazoAutoInlineDisplay(message) {
     //auto inline display(pyazo)
     message = message.replace(/http(s)?:\/\/yairc.cfe.jp(:5000)?(\/[\x21-\x7e]+)\.(png|gif|jpg|jpeg)/gi,
                               "<a href='//yairc.cfe.jp$3.$4' target='_blank'><img src='//yairc.cfe.jp$3.$4' style='max-width:300px;max-height:300px;'/></a>");
@@ -26,13 +26,13 @@ function filterPluginPyazoAutoInlineDisplay(message) {
     return message;
 }
 
-function filterPluginGistInline(message){
+function messageFilterGistInline(message){
     return message.replace(/https:\/\/gist.github.com\/[a-zA-Z0-9\-_]{1,30}\/([0-9a-zA-Z]{1,30})/g, function(whole,s1) {
         return( 'https://gist.github.com/'+s1+'/ <br><iframe data-gist-id="'+s1+'" style="width:100%;"></iframe><script>load_gist("'+s1+'")</script><br>' );
     });
 }
 
-function filterPluginAsamashiate(message) {
+function messageFilterAsamashiate(message) {
     //アサマシエイト （サーバー運営費に当てます！！！）
     message = message.replace(/\/\/www\.amazon\.co\.jp[\x21-\x7e]*\/dp\/([0-9A-Z]{10,13})[\x21-\x7e]*/ig, function(whole,s1) {
         return( whole+' <br><iframe src="//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&t=uzulla-22&o=9&p=8&l=as4&m=amazon&f=ifr&ref=ss_til&asins='+s1+'" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>' );
@@ -43,25 +43,25 @@ function filterPluginAsamashiate(message) {
     return message;
 }
 
-function filterPluginTwitterInline(message){
+function messageFilterTwitterInline(message){
     //twitter inline https://twitter.com/uzulla/status/389391040480051200
     return message.replace(/http[s]*:(\/\/twitter.com\/[a-zA-Z0-9\-_]{1,40}\/status[e|s]*\/[0-9]{1,40})/g, function(whole,s1) {
         return( 'https:'+s1+' <br><blockquote class="twitter-tweet"><a href="'+s1+'"></blockquote><script>twttr.widgets.load();</script>' );
     });
 }
 
-function filterPluginYoutubeThumbnail(message){
+function messageFilterYoutubeThumbnail(message){
     // YT thumbnail
     return message.replace(/http(s)?:\/\/www.youtube.com\/[\x21-\x7e]*v=([a-zA-Z0-9\-]+)/g,
                               "<img src='//i1.ytimg.com/vi/$2/default.jpg'><br />http://www.youtube.com/watch?v=$2");
 }
 
-function filterPluginAutoLink(message){
+function messageFilterAutoLink(message){
     //auto link
     return message.replace(/(http(s)?:\/\/[\x21-\x7e]+)/gi, "<a href='$1' target='_blank'>$1</a>");
 }
 
-function filterPluginSyntaxHilight(message) {
+function messageFilterSyntaxHilight(message) {
     message = message.replace(/&#62;\|javascript\|\n([\s\S]*?)\n\|\|&#60;/g, function(whole,s1) {
         return( '<pre class="sh_javascript">' + s1 + '</pre>' );
     });
