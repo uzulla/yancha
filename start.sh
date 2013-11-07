9d95712fbb810c9810149facf887d3f21374cb09
moznion <moznion@gmail.com>
2013-11-07 22:01:21 +0900

    YANCHA_DEBUGオンにしてstart.sh実行すると-rで実行される

--- start.sh
+++ start.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-carton exec -- twiggy -l :3000 chat.psgi
+
+twiggy_opt="-l :3000"
+if [ "$YANCHA_DEBUG" != "" ] ; then
+  if [ $YANCHA_DEBUG -gt 0 ] ; then
+    twiggy_opt="$twiggy_opt -r"
+  fi
+fi
+carton exec -- twiggy $twiggy_opt chat.psgi
+
 #export YANCHA_DEBUG=1
 #carton exec -- twiggy -l :3000 --access-log access_log chat.psgi &
 #echo $! > yancha.pid
