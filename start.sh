1649af2072f39c9e071a0efd46e6d576fa87a1f1
uzulla <uzulla@himitsukichi.com>
2015-12-20 13:36:26 +0900

    pull from prod server

--- start.sh
+++ start.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
-
-twiggy_opt="-l :3000"
-if [ "$YANCHA_DEBUG" != "" ] ; then
-  if [ $YANCHA_DEBUG -gt 0 ] ; then
-    twiggy_opt="$twiggy_opt -r"
-  fi
-fi
-carton exec -- twiggy $twiggy_opt chat.psgi
-
-#export YANCHA_DEBUG=1
-#carton exec -- twiggy -l :3000 --access-log access_log chat.psgi &
-#echo $! > yancha.pid
+#carton exec -- twiggy -l :3000 chat.psgi
+#export PERL_ANYEVENT_LOG="filter=trace:log=+%file:%file=file=/tmp/mylog"
+export YANCHA_DEBUG=1
+#export POCKETIO_HANDLE_DEBUG=1
+#export POCKETIO_CONNECTION_DEBUG=1
+#export POCKETIO_POOL_DEBUG=1
+#export POCKETIO_RESOURCE_DEBUG=1
+#export POCKETIO_DEBUG=1
+carton exec -- twiggy -l :3000 --access-log access_log chat.psgi &
+echo $! > yancha.pid
